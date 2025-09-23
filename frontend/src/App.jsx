0a34df1b030a3d22478b976986b3f8afc786ecba
ALIGHASSOUN <alighassoun9@gmail.com>
2025-09-23 17:10:14 +0300

    FIRST COMMIT

---
+++ frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+import Order from "./pages/Order";
+import PlaceOrder from "./pages/PlaceOrder";
+import Product from "./pages/Product";
+
+const App = () => {
+  return (
+    <div className="px-4 sm:px-[5vm] md:px-[7vw] lg:px-[9vw]">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/order" element={<Order />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/product" element={<Product />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
