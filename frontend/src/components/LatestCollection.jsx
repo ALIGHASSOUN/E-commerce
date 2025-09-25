be77c537c4b2fa1e96350cdded997ba82a35c2ed
ALIGHASSOUN <alighassoun9@gmail.com>
2025-09-25 22:36:00 +0300

    fixing a bug

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
 
 const LatestCollection = () => {
-  return <div>LatestCollection</div>;
+  const { products } = useContext(ShopContext);
+  const [latestProducts, setLatestProducts] = useState([]);
+  useEffect(() => {
+    setLatestProducts(products.slice(0, 10));
+  }, []);
+  console.log(products);
+  return (
+    <div className="my-10">
+      <div className="text-center py-8 text-3xl">
+        <Title text1={"LATEST"} text2={"COLLECTION"} />
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+          shaker shaker shalerdfd dfd dd ff gf
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default LatestCollection;
