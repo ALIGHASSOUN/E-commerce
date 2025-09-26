502b5a24f5c63f954613c74c87c0575d66e5fc9e
ALIGHASSOUN <alighassoun9@gmail.com>
2025-09-26 18:05:48 +0300

    latest collection

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useContext } from "react";
 import { ShopContext } from "../context/ShopContext";
 import Title from "./Title";
+import ProductItem from "./ProductItem";
 
 const LatestCollection = () => {
   const { products } = useContext(ShopContext);
@@ -17,6 +18,18 @@ const LatestCollection = () => {
         <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
           shaker shaker shalerdfd dfd dd ff gf
         </p>
+      </div>
+      {/* Rendering Products */}
+      <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {latestProducts.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._id}
+            image={item.image}
+            name={item.name}
+            price={item.price}
+          />
+        ))}
       </div>
     </div>
   );
