481b3798819f458271848bdcfa5824b19c416a09
ALIGHASSOUN <alighassoun9@gmail.com>
2025-10-04 22:57:32 +0300

    editing some code

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { useParams } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
 
 const Product = () => {
+  const { productId } = useParams();
+  const { products } = useContext(ShopContext);
+  const [productData, setProductData] = useState(false);
+
+  const fetchProductData = async () => {
+    products.map((item) => {
+      if (item._id === productId) {
+        setProductData(item);
+        return null;
+      }
+    });
+  };
   return <div>Product</div>;
 };
 
