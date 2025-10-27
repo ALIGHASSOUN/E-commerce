8c26f8546848a00cd326b6402da64e4f408f86b1
ALIGHASSOUN <alighassoun9@gmail.com>
2025-10-27 16:07:58 +0300

    moving forward in back end

---
+++ backend/controllers/userController.js
@@ -0,0 +1,19 @@
+import userModel from "../models/UserModel.js";
+
+// Route for user login
+const loginUser = async (req, res) => {};
+
+// Route for user register
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    //checking user already exists or not
+    const exists = await userModel.findOne({ email });
+  } catch (error) {}
+};
+
+//Route for admin login
+const adminLogin = async (req, res) => {};
+
+export { loginUser, registerUser, adminLogin };
