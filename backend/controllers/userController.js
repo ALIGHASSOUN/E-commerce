5ff32bdfe996a207653280ad2ab300b22b48c851
ALIGHASSOUN <alighassoun9@gmail.com>
2025-10-31 19:18:23 +0300

    moving forward with back end

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,31 @@
+import validator from "validator";
 import userModel from "../models/UserModel.js";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
 
 // Route for user login
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.json({ success: false, message: "User doesn't exists" });
+    }
+    const isMatch = await bcryptjs.compare(password, user.password);
+    if (isMatch) {
+      const token = createToken(user._id);
+      res.json({ success: true, token });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // Route for user register
 const registerUser = async (req, res) => {
@@ -11,9 +35,41 @@ const registerUser = async (req, res) => {
     //checking user already exists or not
     const exists = await userModel.findOne({ email });
     if (exists) {
-      return res.json({ success: false, message: "User already" });
+      return res.json({ success: false, message: "User already exists" });
     }
-  } catch (error) {}
+
+    //validate email formal and strong password
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "Please enter a valid email",
+      });
+    }
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "please enter a strong password",
+      });
+    }
+    // hashing user password
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    const newUser = new userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const user = await newUser.save();
+
+    const token = createToken(user._id);
+
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
 };
 
 //Route for admin login
